8e4d6f40b772829c91ed95151b72b510ae732a30
Gleb Linnik <gleb_linnik@mail.ru>
2018-10-04 16:38:08 +0300

    Project initial commit

---
+++ app/src/main/java/com/app/beb/bebapp/BebActivity.java
@@ -0,0 +1,13 @@
+package com.app.beb.bebapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beb);
+    }
+}
