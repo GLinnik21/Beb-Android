8285f9ab12879e83e8ff1e6fc5738bb9b9064d69
Gleb Linnik <gleb_linnik@mail.ru>
2018-12-02 13:35:48 +0300

    Bottom bar navigation

--- app/src/main/java/com/app/beb/bebapp/MainActivity.java
+++ app/src/main/java/com/app/beb/bebapp/MainActivity.java
@@ -8,25 +8,17 @@ import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import static androidx.navigation.ui.NavigationUI.setupWithNavController;
 
 public class MainActivity extends AppCompatActivity
 implements Profile.OnFragmentInteractionListener {
-
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +26,8 @@ implements Profile.OnFragmentInteractionListener {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        setupWithNavController(navigation, navController);
     }
 
     @Override
